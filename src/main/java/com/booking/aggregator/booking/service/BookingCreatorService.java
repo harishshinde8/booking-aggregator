340d987562046389ab8e54c856df66719c2597e5
harishshinde8 <harishshinde8@gmail.com>
2025-11-13 16:31:36 +0400

    main | CHORE | Remove unwanted changes

--- src/main/java/com/booking/aggregator/booking/service/BookingCreatorService.java
+++ src/main/java/com/booking/aggregator/booking/service/BookingCreatorService.java
@@ -6,20 +6,10 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import java.security.SecureRandom;
 
-import static com.booking.aggregator.booking.constants.BookingConstants.ALPHANUMERIC;
-
 @Service
 public interface BookingCreatorService {
 
     SecureRandom RANDOM = new SecureRandom();
 
     Mono<Trip> createBooking(CreateBookingRequest request);
-
-    static String generatePnr(int length) {
-        StringBuilder sb = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            sb.append(ALPHANUMERIC.charAt(RANDOM.nextInt(ALPHANUMERIC.length())));
-        }
-        return sb.toString();
-    }
 }
